042505e6f1cc6b47b1dab1d5b10fa653246efb17
Jitendra <jitendra.abes@gmail.com>
2019-04-20 18:37:01 +0530

    Checkin after MVN build prin more sysout- error resolved

--- src/main/java/com/mastercard/jenkin/App.java
+++ src/main/java/com/mastercard/jenkin/App.java
@@ -10,5 +10,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World From Jenkin!" );
+        System.out.println( "Successfully build and executed using Jenkin jobs " );
     }
 }
